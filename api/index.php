78ae5a72b928d638a20a4b4c8bd90b0790be9ce5
V21-N <vinnr21@gmail.com>
2026-06-07 23:34:09 +0700

    Fix: Standarisasi api index untuk Laravel 13

--- api/index.php
+++ api/index.php
@@ -1,4 +1,17 @@
 <?php
 
-// Jembatan khusus untuk mengesekusi Laravel di Vercel Serverless
-require __DIR__ . '/../public/index.php';
+// 1. Arahkan ke autoload milik composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// 2. Arahkan ke bootstrap Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
