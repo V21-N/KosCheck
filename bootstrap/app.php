f39e5b7d936e0cac9e1dc28221a0fac02a84914b
V21-N <vinnr21@gmail.com>
2026-06-08 00:23:56 +0700

    Fix: Paksa nyalakan debug mode untuk lacak eror internal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// PAKSA NYALAKAN DEBUG DI VERCEL
+putenv('APP_DEBUG=true');
+putenv('APP_ENV=local');
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
